3ba6a8b6f29d17b2fe2dc26320d6f1ce30a14806
FanZouGit <fanzou1@gmail.com>
2019-08-25 04:17:52 +0000

    br1

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -10,4 +10,11 @@ public class TestBankAccount {
 		double amount = account.debit(5);
 		Assert.assertEquals(5.0, amount);	
 	}
+	@Test
+	public void testNoDebit()
+	{	
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(25);
+		Assert.assertEquals(10.0, amount);	
+	}
 }
