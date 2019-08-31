fd0bc04dd587a68c960317292271e57cbe62148d
Fan Zou <fanzou@Fans-MacBook-Pro.local>
2019-08-30 22:03:27 -0400

    add anotehr case

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -9,6 +9,10 @@ public class BankAccount {
   public double debit(double amount) {
     if (balance < amount) {
       amount = balance;
+      if ( balance*2 < amount)
+      {
+        return -1;
+      }
     }
 
     balance -= amount;
