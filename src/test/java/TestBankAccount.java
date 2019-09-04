51e01b68237ba0b64ddfea01077f741ed32460f0
Fan Zou <fanzou@Fans-MacBook-Pro.local>
2019-09-04 14:18:11 -0400

    delete test

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -10,11 +10,4 @@ public class TestBankAccount {
 		double amount = account.debit(5);
 		Assert.assertEquals(5.0, amount);	
 	}
-	@Test
-	public void testNoDebit()
-	{	
-		BankAccount account = new BankAccount(10);
-		double amount = account.debit(5);
-		Assert.assertEquals(5.0, amount);	
-	}
 }
